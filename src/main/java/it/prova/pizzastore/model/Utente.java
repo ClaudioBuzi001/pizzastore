958f00f9868e9a9da00208f30e671e9a1971da60
ClaudioBuzi001 <buziclaudio@gmail.com>
2022-06-02 17:11:43 +0200

    Correzione entità Utente

--- src/main/java/it/prova/pizzastore/model/Utente.java
+++ src/main/java/it/prova/pizzastore/model/Utente.java
@@ -51,4 +51,150 @@ public class Utente {
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "utente")
 	private Set<Ordine> ordini = new HashSet<>(0);
 
+	public Utente() {
+		super();
+	}
+
+	public Utente(String username, String password, String nome, String cognome, Date dateCreated) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+	}
+
+	public Utente(String username, String password, String nome, String cognome, Date dateCreated, Set<Ordine> ordini) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+		this.ordini = ordini;
+	}
+
+	public Utente(Long id, String username, String password, String nome, String cognome, Date dateCreated) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+	}
+
+	public Utente(Long id, String username, String password, String nome, String cognome, Date dateCreated,
+			Set<Ruolo> ruoli) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+		this.ruoli = ruoli;
+	}
+
+	public Utente(Long id, String username, String password, String nome, String cognome, Date dateCreated,
+			StatoUtente stato, Set<Ruolo> ruoli) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+		this.stato = stato;
+		this.ruoli = ruoli;
+	}
+
+	public Utente(String username, String password, String nome, String cognome, Date dateCreated, StatoUtente stato,
+			Set<Ordine> ordini) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dateCreated = dateCreated;
+		this.stato = stato;
+		this.ordini = ordini;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public StatoUtente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoUtente stato) {
+		this.stato = stato;
+	}
+
+	public Set<Ruolo> getRuoli() {
+		return ruoli;
+	}
+
+	public void setRuoli(Set<Ruolo> ruoli) {
+		this.ruoli = ruoli;
+	}
+
+	public Set<Ordine> getOrdini() {
+		return ordini;
+	}
+
+	public void setOrdini(Set<Ordine> ordini) {
+		this.ordini = ordini;
+	}
+	
+	
+	
+	 
+
 }
